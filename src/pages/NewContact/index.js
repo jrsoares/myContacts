973ee0fbccd14533aad1d4a7bbd4f00d36dc480d
Júnior Soares <juniorsoaresti@gmail.com>
2022-03-03 13:47:25 -0400

    Creating component Input and Select

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,8 +1,16 @@
+import { Input } from '../../components/Input';
+import { Select } from '../../components/Select';
+
 import PageHeader from '../../components/PageHeader';
 
 export default function NewContactPage() {
   return (
-    <PageHeader title="Novo Contato" />
+    <>
+      <PageHeader title="Novo Contato" />
+      <Input placeholder="Nome" />
+      <Select>
+        <option value="1">Instagram</option>
+      </Select>
+    </>
   );
 }
-
